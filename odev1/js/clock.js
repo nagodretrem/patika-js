9c7228f8abd23818f531b168057a935fbc699f57
Mert <nagodretrem@gmail.com>
2022-12-01 16:14:39 +0300

    JavaScript Patikası 1. Odev

---
+++ odev1/js/clock.js
@@ -0,0 +1,19 @@
+function showTime(){
+    let today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+    let day = today.getDay();    
+    let days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+    let dayName = days[day];
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+
+
+    document.getElementById("myClock").innerText = `${hours}:${minutes}:${seconds}  ${dayName}`;
+}
+
+setInterval(showTime, 1000);
